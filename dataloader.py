af4072acd67fd69fe7a6a35e4f34287f70ca7072
spc93 <steve.collins@diamond.ac.uk>
2019-01-09 18:35:13 +0000

    Add files via upload
    Fix indentation (all tabs) and compatibility with Python 3.

--- dataloader.py
+++ dataloader.py
@@ -36,84 +36,84 @@ class pdnx(pd.DataFrame):
             _nx = nx.nxload(filestr,'r')
 
         except:
-            print "=== Error loading file %s" % filestr
+            print("=== Error loading file %s" % filestr)
             return
         
-	_load_dataframe_success = False
-	for scandata_field in scandata_field_list:
-	    try:
+        _load_dataframe_success = False
+        for scandata_field in scandata_field_list:
+            try:
                 nx_scan_dict = dict(_nx[scandata_field])
                 for key in nx_scan_dict.keys():
-		    try:
-                       nx_scan_dict[key] = nx_scan_dict[key].nxdata.flatten()
-		    except:
-		       pass
+                    try:
+                        nx_scan_dict[key] = nx_scan_dict[key].nxdata.flatten()
+                    except:
+                        pass
                 pd.DataFrame.__init__(self, nx_scan_dict)
-		_load_dataframe_success = True
-		break
+                _load_dataframe_success = True
+                break
             except:
-		pass
+                pass
 
-	if not _load_dataframe_success:
-	    #print '=== Failed to create DataFrame from data - create empty DataFrame'
+        if not _load_dataframe_success:
+            #print('=== Failed to create DataFrame from data - create empty DataFrame')
             pd.DataFrame.__init__(self)
 
         setattr(self,'nx',_nx)
-	
-	for scan_command in scan_command_field_list:	
-	    try:
-	        setattr(self, 'scan', filestr+'\n'+_nx[scan_command].nxdata)
-		break
-	    except:
-	        pass
+        
+        for scan_command in scan_command_field_list:	
+            try:
+                setattr(self, 'scan', filestr+'\n'+_nx[scan_command].nxdata)
+                break
+            except:
+                pass
 
 
     def plt(self, *args, **kwargs):
-    	kwargs.setdefault('title', self.scan)
-    	kwargs.setdefault('grid', True)
-    	self.plot(*args, **kwargs)
+        kwargs.setdefault('title', self.scan)
+        kwargs.setdefault('grid', True)
+        self.plot(*args, **kwargs)
 
 
     def _list_to_dot_sep_string(self, lst):
-    	outstr = ''
-    	for item in lst:
-        	outstr += '.' + str(item)
-    	return outstr
+        outstr = ''
+        for item in lst:
+            outstr += '.' + str(item)
+        return outstr
 
     def _find_key(self, tree, key, previous_keys=[]):
         global _keylist
         try:
             for keyval in tree.keys():
-        	if keyval == key or key == '':
-                     _keylist += [previous_keys + [keyval]]
-          	self._find_key(tree[keyval], key, previous_keys = previous_keys + [keyval])
-    	except:
-        	pass
+                if keyval == key or key == '':
+                    keylist += [previous_keys + [keyval]]
+                self._find_key(tree[keyval], key, previous_keys = previous_keys + [keyval])
+        except:
+            pass
 
     def findkeys(self, keystring):
-    	'Return list of key sequences (lists) that end with keystring'    
-    	global _keylist
-    	_keylist=[]
-    	self._find_key(self.nx, keystring)
-    	return _keylist
+        'Return list of key sequences (lists) that end with keystring'    
+        global _keylist
+        _keylist=[]
+        self._find_key(self.nx, keystring)
+        return _keylist
 
     def find(self, keystring=''):
-	'Return nexus fields and values for keystring'
-	for key_sequence in self.findkeys(keystring):
-	    obj = self.nx
-	    for key in key_sequence:
-		obj = obj[key]
-	    print '.nx' + self._list_to_dot_sep_string(key_sequence) + ' : \t', obj
+        'Return nexus fields and values for keystring'
+        for key_sequence in self.findkeys(keystring):
+            obj = self.nx
+            for key in key_sequence:
+                obj = obj[key]
+            print('.nx' + self._list_to_dot_sep_string(key_sequence) + ' : \t', obj)
 
     def pruned_tree(self, depth):
-	'Print pruned tree'
-	allfieldlist = self.findkeys('')
-	previous = []
-	for fieldlist in allfieldlist:
-	    fieldshort = fieldlist[:depth]
-	    if fieldshort != previous:
-	        print self._list_to_dot_sep_string(fieldshort)
-        	previous = fieldshort
+        'Print pruned tree'
+        allfieldlist = self.findkeys('')
+        previous = []
+        for fieldlist in allfieldlist:
+            fieldshort = fieldlist[:depth]
+            if fieldshort != previous:
+                print(self._list_to_dot_sep_string(fieldshort))
+                previous = fieldshort
 
 
 
@@ -127,16 +127,16 @@ def vec2mat(vecx, vecy, vecz, n_inner=None):
     #n_inner: Number of points in inner loop - calculated if not specified
     #Arrays are truncated if the size doesn't match the required shape
 
-    vx=np.array(vecx[:]); vy=np.array(vecy[:]); vz=np.array(vecz[:]) #get inputs in standard form
-    if n_inner==None:   #calculate number in inner loop by looking for jumps
-        jumps=np.abs(np.diff(vx)*np.diff(vy))
-        n_inner=matplotlib.mlab.find(jumps>np.mean(jumps))[0]+1
+    vx = np.array(vecx[:]); vy = np.array(vecy[:]); vz = np.array(vecz[:]) #get inputs in standard form
+    if n_inner == None:   #calculate number in inner loop by looking for jumps
+        jumps = np.abs(np.diff(vx) * np.diff(vy))
+        n_inne = matplotlib.mlab.find(jumps>np.mean(jumps))[0] + 1
 
-    n_outer=len(vx)/n_inner
+    n_outer = len(vx)/n_inner
     #reshape matrices
-    matx=vx[0:n_inner * n_outer].reshape(n_outer,n_inner)
-    maty=vy[0:n_inner * n_outer].reshape(n_outer,n_inner)
-    matz=vz[0:n_inner * n_outer].reshape(n_outer,n_inner)
+    matx = vx[0:n_inner * n_outer].reshape(n_outer,n_inner)
+    maty = vy[0:n_inner * n_outer].reshape(n_outer,n_inner)
+    matz = vz[0:n_inner * n_outer].reshape(n_outer,n_inner)
 
     return matx, maty, matz
 
