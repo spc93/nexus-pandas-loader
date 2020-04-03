3ee578d592c34bda69d2cd9d85e10af8ff3f1b81
spc93 <steve.collins@diamond.ac.uk>
2020-04-03 13:01:07 +0100

    Add files via upload
    New version - uses NXclassic_scan definition to get ordered dataframe from scannable output fields,

--- pdnx.py
+++ pdnx.py
@@ -2,6 +2,7 @@ import nexusformat.nexus as nx
 import pandas as pd
 import matplotlib
 import numpy as np
+#from astropy.modeling.tests import data
 
 pd.set_option('display.max_rows',8)
 pd.set_option('display.max_columns', 500)
@@ -16,7 +17,10 @@ _measurement = '/measurement'
 class pdnx(pd.DataFrame): 
     '''
     nexusformat wrapper: tries to create dataframe from default data
-    whole nexus file is under .nx attribute
+    whole nexus file is under .nx attribute 
+    either: specify entry and data field (data field must contain only data of the same length)
+    or use NXclassic_scan definition (looks only for first entry or subentry in first entry)
+        (the latter converts only fields used by scannables in scan and preserves order) 
     e.g. 
     n=pdnx(p % 633777)  open file for scan 633777 (p is filename/format specifier)
     n                   display pandas dataframe
@@ -35,7 +39,76 @@ class pdnx(pd.DataFrame):
 
     '''
 
-    def __init__(self,  filestr, entry = _entry, measurement = _measurement):
+
+    def __init__(self,  filestr, entry = _entry, data = _measurement):
+        '''
+        entry = select nexus entry for measurement data and set default to this entry
+        data = nexus field containing datafor pandas dataframe
+        '''
+        try:
+            _nx = nx.nxload(filestr,'r')
+
+        except:
+            print("=== Error loading file %s" % filestr)
+            return
+        
+        _load_dataframe_success = False
+        _use_classicscan = False
+
+        entrydata = None
+        if not entry == None and not data == None:
+            entrydata = entry+data
+        else:
+            entry =  getNexusSubentryWithDefinition(_nx, definition = 'NXclassic_scan')
+            for key in _nx[entry].keys():
+                if 'NXdata' in str(type(_nx[entry][key])):
+                    entrydata = '%s/%s' % (entry, key)
+                    _use_classicscan = True
+
+        try:
+            _nx[entrydata]
+        except:
+            raise ValueError('=== Problem finding data field')
+
+        try:
+            if _use_classicscan:
+                keys = _nx[entrydata]['scan_fields'] # use fields from scan_fields required by NXclassic_scan
+            else:
+                keys = _nx[entrydata].keys()        # use all fields - must all be the same length to avoid an error
+
+            nx_scan_dict = {}
+
+            for key in keys:
+                try:
+                    nx_scan_dict[key] = _nx[entrydata][key].nxdata.flatten()
+                except:
+                    pass
+            pd.DataFrame.__init__(self, nx_scan_dict, columns = keys)   ###############
+    
+            _load_dataframe_success = True
+        except:
+            pass
+
+        try:
+            _nx['default'] = entry #set default entry to specified entry (for files with multiple enties)
+        except:
+            pass
+
+        if not _load_dataframe_success:
+            print('=== Failed to create DataFrame from data - create empty DataFrame')
+            pd.DataFrame.__init__(self)
+
+        setattr(self,'nx',_nx)
+
+        try:
+            setattr(self, 'scan', filestr+'\n'+_nx[entry]['title'].nxdata)
+        except:
+            pass
+
+
+
+
+    def __init_old__(self,  filestr, entry = _entry, measurement = _measurement):
         '''
         entry = select nexus entry for measurement data and set default to this entry
         measurement = nexus field containing datafor pandas dataframe
@@ -65,7 +138,6 @@ class pdnx(pd.DataFrame):
             _nx['default'] = entry #set default entry to specified entry (for files with multiple enties)
         except:
             pass
-
 
         if not _load_dataframe_success:
             #print('=== Failed to create DataFrame from data - create empty DataFrame')
@@ -128,7 +200,41 @@ class pdnx(pd.DataFrame):
                 previous = fieldshort
 
 
+def getNexusSubentryWithDefinition(nxroot, definition = None):
+    '''
+    return NeXus tree branch string that is an entry or subentry containing the specified definition (string)
+    if no definition specified then the function displays all the definitions found
+    '''
+    field_with_definition = None
+    all_definitions = []
 
+    for entry in nxroot.keys():                             # loop through each entry
+        try:
+            defn = str(nxroot[entry]['definition'])
+            all_definitions += [defn]
+            if defn == definition:
+                #field_with_definition = nxroot[entry]           # if it has required definition then break
+                field_with_definition = '/%s' % entry 
+                break
+        except:
+            pass
+    
+        for subentry in nxroot[entry].keys():               # loop through each field in entry
+            if 'NXsubentry' in str(type(nxroot[entry][subentry])): # if it is a subentry ...
+                try:
+                    defn = str(nxroot[entry][subentry]['definition'])
+                    all_definitions += [defn]
+                    if defn == definition:  # check if it has the required definition
+                        #field_with_definition = nxroot[entry][subentry]
+                        field_with_definition = '/%s/%s' % (entry, subentry) 
+                        break
+                except:
+                    pass
+                
+    if definition == None:
+        print(all_definitions)
+        
+    return field_with_definition
 
 
 def vec2mat(vecx, vecy, vecz, n_inner=None):
@@ -155,4 +261,13 @@ def vec2mat(vecx, vecy, vecz, n_inner=None):
 
 
 
+############ testing - delete ##################
+#filenum = 815893
+#p='/dls/science/users/spc93/misc_nexus_data/modified/%i.nxs'
+#n=pdnx(p % 815893)
+#n=pdnx(p % 815893, entry = '/entry1/scan/', data = None)
+#n=pdnx(p % 815893, entry = None, data = None) # use classic_scan
+#n=pdnx(p % 815893) # old method
+
+##################
 
